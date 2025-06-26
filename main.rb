ff1f51a9de092a74a0db647ab542d43254f62b55
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-06-26 17:57:13 +0200

    Made custom flatten method

--- main.rb
+++ main.rb
@@ -36,4 +36,42 @@ def beer_bottles(n)
   end
 end
 
-beer_bottles(5)
+# beer_bottles(5)
+
+# method to return result of fibonacci sequence
+def fibonacci(n)
+  # base case
+  return 0 if n == 0
+  return 1 if n == 1
+  # recursive case
+  if n > 1
+    fibonacci(n-1) + fibonacci(n-2)
+  end
+end
+
+# puts fibonacci(5)
+
+=begin
+Analogy:
+Think of it like unpacking nested boxes:
+If you open a box and find a number: great! Put it in your result bag.
+If you open a box and find more boxes: recursively 
+open each until all contents are out.
+.push (or <<) would push the entire box to flat, not what you want!
+.concat pushes item by item, what you want!
+=end
+
+# method to flatten an array, thus making the array 1D
+def flatten_array(arr)
+  flat = []
+  arr.each do |item|
+    if item.is_a?(Array)
+      flat.concat(flatten_array(item))
+    else
+      flat << item
+    end
+  end
+  return flat
+end
+
+p flatten_array([[1, 2], [3, 4]])
