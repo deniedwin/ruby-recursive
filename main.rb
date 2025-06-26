a17253a3d08ed9cda0b77da0b1f4b4ae94bf9c14
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-06-26 16:22:34 +0200

    recursive beer bottles

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
-# This project is to train recursive methods
+# This project is to train using recursive methods
+
+# method to output factorial of a number n!
 def factorial(n)
   if n == 0
     return 1
@@ -9,6 +11,7 @@ end
 
 # puts factorial(5)
 
+# method to return true if input is palindrome or not
 def palindrome(word)
   # base case
   return true if word.length <= 1
@@ -20,4 +23,17 @@ def palindrome(word)
   end
 end
 
-puts palindrome('racecar')
+# puts palindrome('racecar')
+
+# method to print number and then return n-1 each time
+def beer_bottles(n)
+  # base case
+  return 0 if n <= 0
+  # recursive case
+  if n > 0
+    puts "beer bottles: #{n}"
+    beer_bottles(n-1)
+  end
+end
+
+beer_bottles(5)
