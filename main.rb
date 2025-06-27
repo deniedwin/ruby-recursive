366bc3e824b7142e048855207e43a2e2f3eeb3b2
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-06-27 16:22:40 +0200

    Made integer to roman number method recursion

--- main.rb
+++ main.rb
@@ -52,6 +52,7 @@ end
 # puts fibonacci(5)
 
 =begin
+method to flatten an array, thus making the array 1D
 Analogy:
 Think of it like unpacking nested boxes:
 If you open a box and find a number: great! Put it in your result bag.
@@ -60,8 +61,6 @@ open each until all contents are out.
 .push (or <<) would push the entire box to flat, not what you want!
 .concat pushes item by item, what you want!
 =end
-
-# method to flatten an array, thus making the array 1D
 def flatten_array(arr)
   flat = []
   arr.each do |item|
@@ -74,4 +73,61 @@ def flatten_array(arr)
   return flat
 end
 
-p flatten_array([[1, 2], [3, 4]])
+# p flatten_array([[1, 2], [3, 4]])
+
+=begin
+method to change integer to roman number
+hash map roman_mapping was given
+input number -> roman number
+example:
+n = 1954 -> M CM L IV
+roman = ''
+base = 1000
+n / base = 1,..
+n % base = 954
+1,.. * base = 1000
+1000 in map? yes -> 'M'
+roman.push('M')
+n = 954
+base /= 10
+
+n / base = 9,..
+n % base = 54
+9,.. * base = 900
+900 in map? yes -> 'CM'
+roman.push('CM')
+n = 54
+base /= 10
+etc..
+=end
+
+
+def to_roman(n)
+  roman_mapping = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+  }
+
+  # base case
+  return '' if n == 0
+
+  # recursive case
+  roman_mapping.each do |key, value|
+    if key <= n
+      return value + to_roman(n - key)
+    end
+  end
+end
+
+puts to_roman(2878)
